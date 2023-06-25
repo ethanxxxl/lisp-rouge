1ab9f0bcbfb5e5d55660a7df75e03411f153be53
Ethan Smith <ethansmith.dev@gmail.com>
2023-06-25 17:43:14 -0500

    fixed errors in BMP code. It should be working well now

--- noise.lisp
+++ noise.lisp
@@ -83,6 +83,21 @@
   "returns (x y) where x and y are the dimensions given by index"
   (list (mod index width) (floor (/ index width))))
 
+(defun perlin-map (height width step &optional (state *random-state*))
+  (declare (fixnum height width) ((float 0.0 1.0) step)
+           (optimize (speed 3)))
+
+  ;; create an array large enough to hold all perlin values.
+  (let ((val-map (make-array (list (round (/ height step))
+                                   (round (/ width step))))))
+    (map-array (lambda (_ i)
+                 (declare (ignore _))
+                 (let* ((subscripts (array-subscripts val-map i))
+                        (x (float (/ (first subscripts) width)))
+                        (y (float (/ (second subscripts) height))))
+                   (perlin x y state)))
+               val-map t)))
+
 (defun array-subscripts (a index)
   "returns a list of subscripts for the element in `A' at `INDEX'. It is
 essentially the inverse of `ARRAY-ROW-MAJOR-INDEX'
@@ -131,8 +146,10 @@ takes it's input"
                      ;; do the new mapping
                      (val (1d-to-2d s1 dims)))
 
-                  (setf s1 (first val)) ; set the index for the next iteration
-                  (car (last val)))))   ; retun the subscript for this dimension
+                  ;; set the index for the next iteration
+                  (setf s1 (first val))
+                  ;; return the subscript for this dimension
+                  (car (last val)))))
 
 (defun map-array (f a &optional include-index)
   "Applies `F' to every element in `A' when rank of `A' is greater than 1. returns
@@ -150,20 +167,6 @@ additional parameter"
                    (funcall f (row-major-aref a i) (when include-index i))))
     results))
 
-(defun perlin-map (height width step &optional (state *random-state*))
-  (declare ((integer 0) height width) ((float 0.0 1.0) step))
-
-  ;; create an array large enough to hold all perlin values.
-  (let ((val-map (make-array (list (round (/ height step))
-                                   (round (/ width step))))))
-    (map-array (lambda (_ i)
-                 (declare (ignore _))
-                 (let* ((subscripts (array-subscripts val-map i))
-                        (x (float (/ (first subscripts) width)))
-                        (y (float (/ (second subscripts) height))))
-                   (perlin x y state)))
-               val-map t)))
-
 (defun byte-list (bytes num)
   "creates a list of bytes (little endian) from number. The list will be `BYTES'
 long"
@@ -174,13 +177,25 @@ long"
         collect (ldb (byte 8 (* 8 i)) num)))
 
 (defun array-bmp (a path)
-  "writes the contents of array `A' to the file specified at `PATH' as a bitmap"
-  (declare (optimize (debug 3)))
+  "writes the contents of array `A' to the file specified at `PATH' as a bitmap.
+`A' must be a 2D array containing either floats for monochrome images or lists
+of floats, for RGB pixels. Values should range from 0.0 to 1.0, but the function
+will not fail on negative values. It simply takes the absolute value of those
+values.
+
+If the file specified at `PATH' exists, it is superceded by the data in `A'"
+  (declare ((array float 2) a))
+  (declare (optimize (speed 3)))
+
+  ;; first, define the bitmap header fields. The number of bytes for each header
+  ;; is very specific, so each value in the header lists is representative of a
+  ;; single byte.
   (let* ((file-header-size 14)
          (bmpcoreheader-size 12)
          (data-array-size (* 8 (array-total-size a)))
+         ;; create the file header from the elements defined above
          (file-header (concatenate 'list
-                                   '(#x42 #x4D)
+                                   '(#x42 #x4D) ; BMP file header magic number
                                    (byte-list 4 (+ file-header-size
                                                    bmpcoreheader-size
                                                    data-array-size))
@@ -188,40 +203,106 @@ long"
                                    (byte-list 4 (+ file-header-size
                                                    bmpcoreheader-size))))
          (bmpcoreheader (concatenate 'list
+                                     ;; The core header is the simplest BMP
+                                     ;; header
                                      (byte-list 4 bmpcoreheader-size)
+                                     ;; WIDTH
                                      (byte-list 2 (array-dimension a 0))
+                                     ;; HEIGHT
                                      (byte-list 2 (array-dimension a 1))
+                                     ;; PLANES (should always be 1)
                                      (byte-list 2 1)
+                                     ;; BITCOUNT, we are using 3 8 bit channels.
                                      (byte-list 2 24))))
-    (with-open-file (f path :direction :output
-                                      :if-exists :supersede
-                                      :element-type 'unsigned-byte)
 
+    ;; open the file stream. append header information, then the array data.
+    ;; existing files are overwritten.
+    (with-open-file (f path :direction :output
+                            :if-exists :supersede
+                            :element-type 'unsigned-byte)
       (write-sequence file-header f)
       (write-sequence bmpcoreheader f)
+
+      ;; append the array data to the file.
       (loop for i below (array-total-size a)
-            do (write-sequence (let* ((val (row-major-aref a i))
-                                      (uint (round (abs (* 255 val))))
-                                      (width (array-dimension a 0))
-                                      (padding (- 4 (mod (* 3 width) 4))))
+            ;; write all the data in row-major order. Rows must be word aligned,
+            ;; and may need padding at the end of every row.
+            do (write-sequence
+                (let* ((val (row-major-aref a i))
+                       (width (array-dimension a 0))
+                       ;; calculate number of padding bytes.
+                       (padding (- 4 (mod (* 3 width) 4))))
 
-                                 (append
-                                  (list uint uint uint)
+                  ;; append data with any padding bytes
+                  (append
+                   ;; map the list of RGB floats to an 8-bit integer
+                   (mapcar (lambda (n) (round (abs (* 255 n))))
+                           ;; because `A' can be either a float or list, ensure
+                           ;; that it is a list of floats either way, before
+                           ;; passing it to the mapping function
+                           (if (typep val 'list)
+                               val
+                               (make-list 3 :initial-element val)))
 
-                                  ;; add padding at end of line
-                                  (when (and (equal (second (array-subscripts a i))
-                                                    (1- width))
-                                             (/= padding 0))
-                                    (make-list padding :initial-element 0))))
-                               f))
+                   ;; add padding at end of line
+                   (when (and (equal (second (array-subscripts a i))
+                                     (1- width))
+                              (/= padding 0 4)) ; no padding is needed for
+                                                ; either 0 or 4 bytes
+                     (make-list padding :initial-element 0))))
+                f))
       (close f))))
 
 (defun color-test-array (s &optional vert)
+  "returns an array containing a black and white gradient in the direction
+specified by `VERT'. Ie, if `VERT' is t, then it will be a vertical gradient.
+Otherwise, the gradient is horizontal. The size of the array is controlled by
+`S'.
+
+The array is composed of single float values ranging from 0.0 to 1.0"
+
+  ;; create results array to act as an accumulator
   (let ((results (make-array (list s s))))
+    ;; loop through the entire array in row-major order
     (loop for i below (array-total-size results)
+          ;; w is the x or y axis, depending on `VERT'
           do (let* ((w (if vert
                            (first (array-subscripts results i))
                            (second (array-subscripts results i))))
                     (val (float (/ w s))))
                (setf (row-major-aref results i) val)))
     results))
+
+(defun color-test-array2 (s)
+  "Creates a checker pattern with 3 different colors. S controls the size of the square.
+This function returns an array of pixels, where each pixel is a list of RGB
+values."
+
+  ;; create the results array, to be used as an accumulator
+  (let ((results (make-array (list s s))))
+    ;; loop through the entire array in row-major order.
+    (loop for i below (array-total-size results)
+          ;; keep track of x and y each iteration by getting the the subscripts
+          ;; from the index
+          do (let* ((y (first (array-subscripts results i)))
+                    (x (second (array-subscripts results i)))
+                    ;; val is the pixel value. iit depends on the position
+                    ;; position of the pixel in the array.
+                    (val (cond ((and (evenp (round x 10))
+                                     (evenp (round y 10)))
+                                (hex-to-rgb #xAB3229))
+                               ((and (oddp (round x 10))
+                                     (oddp (round y 10)))
+                                (hex-to-rgb #x359CCC))
+                               (t
+                                (hex-to-rgb #x6B605F)))))
+               ;; set the value in the array to the chosen val.
+               (setf (row-major-aref results i) val)))
+    results))
+
+(defun hex-to-rgb (hex)
+  "takes an RGB hex value of the form #xAABBCC. Returns a list of RGB floats
+ranging from 0.0 to 1.0.
+
+ex: (hex-to-rgb #xAABBCC) => (0.8 0.73333335 0.6666667)"
+  (mapcar (lambda (h) (float (/ h 255))) (byte-list 3 hex)))
